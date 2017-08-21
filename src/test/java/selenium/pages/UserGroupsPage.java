01bbc7e5a720afa2cfd6c6c329a710438bfedee4
Victor <viktor.iurkov@yandex.ru>
2017-08-21 20:17:06 +0300

    21 08 17
    bug fix for Users page + user group page

--- src/test/java/selenium/pages/UserGroupsPage.java
+++ src/test/java/selenium/pages/UserGroupsPage.java
@@ -65,10 +65,82 @@ public class UserGroupsPage extends BasePageClass {
     }
 
     @Step("Apply filter")
-    public void applyFilter(String searchRequest)
-    {
+    public void applyFilter(String searchRequest) {
         filterField.clear();
         filterField.inputText(searchRequest);
+    }
+
+    @Step("Delete selected group")
+    public void deleteUser(String name){
+        table.selectElementCheckboxInTable(name);
+        deleteBtn.click();
+        modalConfirmWindow.confirmAction();
+        waitForJSload();
+    }
+
+    @Step("Sort table entries by column name")
+    public void sortTableBy(String columnName){
+        table.sortBy(columnName);
+        waitForJSload();
+    }
+
+    @Step("Select all groups and delete them")
+    public void deleteAllusers(){
+        table.selectAllInTable();
+        deleteBtn.click();
+        modalConfirmWindow.confirmAction();
+        waitForJSload();
+    }
+
+    @Step("Click on 'show inactive' button")
+    public void showInactive(){
+        showInactiveBtn.click();
+        waitForJSload();
+    }
+
+    @Step("Select group and click 'deactivate' button")
+    public void deactivateGroup(String userName){
+        table.selectElementCheckboxInTable(userName);
+        deactivateBtn.click();
+        modalConfirmWindow.confirmAction();
+        waitForJSload();
+    }
+
+    @Step("Select group and click 'activate' button")
+    public void activateGroup(String userName){
+        table.selectElementCheckboxInTable(userName);
+        activateBtn.click();
+        modalConfirmWindow.confirmAction();
+        waitForJSload();
+    }
+
+    @Step("Click cancel btn in user creation dlg")
+    public void newUserGroupCreationCancelling(){
+        crtNewUserGroupCancelBtn.click();
+        waitForJSload();
+    }
+
+    public int countAllElementsInTable(){
+        return table.countAllElementsInTable();
+    }
+
+    public boolean isSortedAscendant(String elementName)
+    {
+        return table.checkAscendantOrderInTable(elementName);
+    }
+
+    public boolean isSortedDescendant(String elementName)
+    {
+        return table.checkDescendantOrderInTable(elementName);
+    }
+
+    public boolean checkElementPresentInTable(String elementName) {
+        return table.tableContainsElements(elementName);
+    }
+
+    public int countElementsInTableByName(String elementName){
+        return table.countElementsInTable(elementName);
+        //countElementsInTable(tableBody, elementName);
     }
 
 
