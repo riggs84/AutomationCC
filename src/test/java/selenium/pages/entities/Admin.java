3e852405ac9b1689fedb24d77659984b430e18a3
riggs84 <victor.iurkov@gmail.com>
2017-08-29 17:57:44 -0400

    28 08 17
    i assume that adding wait until inputfield is focused will solve problem on very fast PC

---
+++ src/test/java/selenium/pages/entities/Admin.java
@@ -0,0 +1,57 @@
+package selenium.pages.entities;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import selenium.Elements.Button;
+import selenium.Elements.Selection;
+import selenium.webtestsbase.BasePageClass;
+
+public class Admin extends BasePageClass {
+    @FindBy(xpath = ".//*[@id='btn-edit-group-admin']")
+    Button editAdministratorBtn;
+
+    @FindBy(xpath = ".//*[@id='page-content-wrapper']//tbody//tr//span[@id='span-admin-id']")
+    WebElement id;
+
+    @FindBy(xpath = ".//*[@id='page-content-wrapper']//tbody//tr//span[@id='span-admin-name']")
+    WebElement adminName;
+
+    @FindBy(xpath = ".//*[@id='page-content-wrapper']//tbody//tr//span[@id='span-admin-email']")
+    WebElement adminEmail;
+
+    @FindBy(xpath = ".//*[@id='page-content-wrapper']//tbody//tr//span[@id='span-email-confirmed']")
+    WebElement emilConfirmed;
+
+    @FindBy(xpath = ".//*[@id='page-content-wrapper']//tbody//tr//span[@id='span-role']")
+    WebElement role;
+
+    @FindBy(xpath = ".//*[@id='admin-edit']//fieldset//select[@name='role']")
+    Selection roleChangeSelect;
+
+    @FindBy(xpath = ".//*[@id='admin-edit']//button[1]")
+    Button cancelAdminEditBtn;
+
+    @FindBy(xpath = ".//*[@id='admin-edit']//button[2]")
+    Button saveAdminEditBtn;
+
+    public Admin(String attribute) {
+        super();
+        setPageUrl(attribute);
+    }
+
+    public String getFieldValue(String name){
+        switch (name.toUpperCase()){
+            case "ID":
+                return id.getText();
+            case "ADMINISTRATOR NAME":
+                return adminName.getText();
+            case "ADMINISTRATOR EMAIL":
+                return adminEmail.getText();
+            case "EMAIL CONFIRMED":
+                return emilConfirmed.getText();
+            case "ROLE":
+                return role.getText();
+        }
+        return null;
+    }
+}
