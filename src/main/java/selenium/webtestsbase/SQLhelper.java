97241f01e2ec5bd023c47700fa157abecfdf5a32
Victor <viktor.iurkov@yandex.ru>
2017-10-05 16:54:23 +0300

    05 10 17
    sql fixing

--- src/main/java/selenium/webtestsbase/SQLhelper.java
+++ src/main/java/selenium/webtestsbase/SQLhelper.java
@@ -11,7 +11,7 @@ import java.sql.Statement;
 public class  SQLhelper {
 
     final static String jdbcDriverClass = "com.mysql.jdbc.Driver";
-    final static String dataBaseURL = "jdbc:mysql://localhost/?allowMultiQueries=true";
+    final static String dataBaseURL = "jdbc:mysql://localhost/?allowMultiQueries=true,sendStringParametersAsUnicode=false";
 
     final static String userName = "root";
     final static String password = "123456";
