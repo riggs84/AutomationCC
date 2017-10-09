3279f7e00e0499842acb212e2229a185f895c803
Victor <viktor.iurkov@yandex.ru>
2017-10-09 17:02:48 +0300

    09 10 17
    setup class with Before and After methods

--- src/main/java/selenium/webtestsbase/SQLhelper.java
+++ src/main/java/selenium/webtestsbase/SQLhelper.java
@@ -66,4 +66,44 @@ public class  SQLhelper {
             }
         }
     }
+
+    public static void dropAdminTable(){
+        Connection conn = null;
+        Statement stmt = null;
+        try {
+            Class.forName(jdbcDriverClass);
+            conn = DriverManager.getConnection(dataBaseURL, userName, password);
+            stmt = conn.createStatement();
+            String sql = "CREATE TABLE `Administrators` (\n" +
+                    "  `admin_id`      int unsigned NOT NULL AUTO_INCREMENT COMMENT 'Administrator Id, globally unique, generated',\n" +
+                    "\n" +
+                    "  `company_id`    int unsigned NOT NULL COMMENT 'Company Id, foreign key',\n" +
+                    "  `admin_email`   char(40)     NOT NULL COMMENT 'Admin Email, can change',\n" +
+                    "\n" +
+                    "  `email_confirmed` boolean   NOT NULL DEFAULT 0 COMMENT 'T: admin_email is confirmed, F: admin did not confirm email',\n" +
+                    "  `admin_name`    varchar(60) NOT NULL COMMENT 'Admin Full Name',\n" +
+                    "  `pass_hash`     char(32)    NOT NULL COMMENT 'MD5 hash of User password, hex string',\n" +
+                    "  `perm_password` boolean     NOT NULL DEFAULT 0 COMMENT 'True: permanent password, F: temp generated password',\n" +
+                    "\n" +
+                    "  `is_company_admin` boolean  NOT NULL DEFAULT 0 COMMENT 'True: company admin, F: group admin',\n" +
+                    "  \n" +
+                    "  `email_confirm_hash` char(32) NULL DEFAULT NULL COMMENT 'Field with tmp email + salt hash for email confirmation',\n" +
+                    "    \n" +
+                    "  `is_active`     boolean     NOT NULL DEFAULT '1',\n" +
+                    "  `created_at`    datetime    NOT NULL COMMENT 'Date time when this Admin was created',\n" +
+                    "\n" +
+                    "  PRIMARY KEY (`admin_id`),\n" +
+                    "  UNIQUE KEY  (`admin_email`),\t\t-- Email is globally unique, between companies too\n" +
+                    "\n" +
+                    "  CONSTRAINT `fk_admins_companies`  \n" +
+                    "    FOREIGN KEY (`company_id`)  \n" +
+                    "    REFERENCES `Companies` (`company_id`) ON DELETE CASCADE\n" +
+                    "\n" +
+                    ") ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Administrators table';"
+                    + "INSERT INTO `Administrators` (`admin_id`, `company_id`, `admin_email`, `admin_name`, `pass_hash`, `is_company_admin`, `created_at`, `perm_password`)";
+            stmt.executeUpdate(sql);
+        } catch(Exception ex){
+            ex.getMessage();
+        }
+    }
 }
