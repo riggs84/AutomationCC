3a29ca666c6818078b05dec12a0d80a564bc5f99
owner <viktor.iurkov@yandex.ru>
2018-02-19 08:04:45 -0800

    fix lack of licenses

--- src/test/java/selenium/Helpers/SQLhelper.java
+++ src/test/java/selenium/Helpers/SQLhelper.java
@@ -47,7 +47,7 @@ public class  SQLhelper {
         }
         // create test company and get it's ID in DB
         try {
-            String sql = "INSERT INTO `Companies` (`company_name`, `server_accounts`, `created_at`, `licensed_ws`, `licensed_s`, `virtual_id`) VALUES ('SiberQA', '', NOW(), '1', '1'," +
+            String sql = "INSERT INTO `Companies` (`company_name`, `server_accounts`, `created_at`, `licensed_ws`, `licensed_s`, `virtual_id`) VALUES ('SiberQA', '', NOW(), '2', '2'," +
                     " (SELECT COMPANY_VIRTUAL_ID()));";
             stmt.executeUpdate(sql);
             String sqlGetCompanyId = "SELECT Companies.company_id FROM `Companies` WHERE Companies.company_name='SiberQA' ;";
