0c90e8100aa163863b5ddadee764dd8b9183a949
test <siber>
2018-03-23 16:38:50 +0300

    Roll-back

--- src/main/java/selenium/webtestbase/BasePageClass.java
+++ src/main/java/selenium/webtestbase/BasePageClass.java
@@ -72,11 +72,6 @@ public class BasePageClass {
             /*DriverFactory.getInstance().getWaitHandler().until(ExpectedConditions
                     .attributeToBe(By.xpath("html"), "class", " "));*/
         } catch (NoSuchElementException ex) {
-            try {
-                Thread.sleep(500);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
         }
     }
 }
